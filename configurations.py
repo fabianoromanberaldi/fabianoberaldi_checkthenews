99b26a2722313cdabd651d8b18fcb10138956278
fabianoromanberaldi <fabiano.beraldi@gmail.com>
2024-01-12 13:50:50 -0300

    "months" property updated to 0

--- configurations.py
+++ configurations.py
@@ -3,7 +3,7 @@ class Configuration:
     def __init__(self) -> None:
         self.timezone = "US/Central"
         self.filter_text = "Brazil"
-        self.months = 1
+        self.months = 0
         self.timeout_in_seconds = 10
         self.browser = "chrome"
         self.download_images = True
